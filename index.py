083b3cac89c2eb89ca6605623a1448ba872b765b
Jhonatan <jhonatandaar@gmail.com>
2020-10-18 00:34:49 -0500

    Added clone feature, local repository done ✔

--- index.py
+++ index.py
@@ -6,8 +6,10 @@ import os
 
 CURRENT_PATH = os.getcwd()
 config = configparser.ConfigParser()
-config.read(os.path.join(CURRENT_PATH,
-                         '/'.join(__file__.split('/')[:-1]), 'config.ini'))
+configPath = os.path.join(CURRENT_PATH,
+                          '/'.join(__file__.split('/')[:-1]),
+                          'config.ini')
+config.read(configPath)
 
 if ('AUTH' not in config.sections()):
     config.add_section('AUTH')
@@ -19,10 +21,11 @@ if ('TOKEN' not in config['AUTH']):
         else:
             continue
     config['AUTH']['TOKEN'] = AUTH_TOKEN
-    with open('config.ini', 'w') as configFile:
+    with open(configPath, 'w+') as configFile:
         config.write(configFile)
 
 repositoryName = input('Repository name: ')
+
 payload = {
     "name": repositoryName,
     "homepage": "https://github.com",
@@ -42,18 +45,7 @@ githubRequest = requests.post('https://api.github.com/user/repos',
                                   'Accept': "application/json"
                               }
                               )
+
 sshUrl = json.loads(githubRequest.content.decode('utf8'))['ssh_url']
 
-workingDir = os.path.join(CURRENT_PATH, repositoryName)
-print(__file__)
-
-
-""" WIP
-subprocess.Popen(["mkdir", workingDir])
-subprocess.Popen(["mkdir", repositoryName])
-subprocess.Popen(["git", "-C", repositoryName, "init"])
-subprocess.Popen(["git", "-C", repositoryName,
-                  "remote", "add", "origin", sshUrl])
-subprocess.Popen(
-    ["git", "-C", repositoryName, "pull", "origin", "main"])
-"""
+subprocess.Popen(["git", "clone", sshUrl])
